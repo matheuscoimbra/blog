94c159769cc408cacaf9dc39ea099600558081a9
mateus.morais <mateus.morais@a3solutions.com.br>
2019-08-08 16:15:32 -0300

    end point estatistica

--- src/main/java/com/mc/blog/repositories/EstatisticaRepository.java
+++ src/main/java/com/mc/blog/repositories/EstatisticaRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EstatisticaRepository extends JpaRepository<Estatistica, Long> {
 
-
+    Estatistica findTopByOrderByIdDesc();
 
 }
